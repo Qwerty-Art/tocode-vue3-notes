2ecfa2dde413d39b951a692d140c4c81096b5f78
Qwerty-Art <arty-qwerty@yandex.ru>
2022-07-07 22:29:26 +0300

    Add Form

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,3 +1,22 @@
 <template>
-  Hello home
+  <div class="md-body">
+    <Form @onSubmit="handleSubmit"></Form>
+  </div>
 </template>
+
+<script>
+import Form from "@/components/Notes/Form";
+export default {
+  components: {
+    Form
+  },
+  methods: {
+    handleSubmit(note) {
+      console.log(note)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
